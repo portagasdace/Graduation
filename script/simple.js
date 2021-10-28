1c0e9f66130f37a10f9164ea0466c3567e281521
liuyusheng <liuyusheng@meishubao.com>
2021-10-28 11:16:39 +0800

    提交

--- script/simple.js
+++ script/simple.js
@@ -1,6 +1,8 @@
 const simpleGit = require("simple-git");
-const path = require("path")
+const path = require("path");
+const { pushd } = require("shelljs");
 const git = simpleGit()
 git.add('./*')
 git.commit("提交")
+git.push()
 console.log(git.status(), "simpleGit");
